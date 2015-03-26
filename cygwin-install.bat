0a2726d60fd049b0f4a7e18958de023b431f3be0
Kouki Ooyatsu <kaishuu0123@gmail.com>
2015-03-26 17:00:51 +0900

    Update cygwin-install.bat

--- cygwin-install.bat
+++ cygwin-install.bat
@@ -15,7 +15,7 @@ SET LOCALDIR=%CD%
 SET ROOTDIR=C:/cygwin64
  
 REM -- These are the packages we will install (in addition to the default packages)
-SET PACKAGES=mintty,wget,ctags,diffutils,git,git-completion,git-svn,stgit,mercurial openssh
+SET PACKAGES=mintty,wget,ctags,diffutils,git,git-completion,git-svn,stgit,mercurial,openssh,zip,unzip
 REM -- These are necessary for apt-cyg install, do not change. Any duplicates will be ignored.
 SET PACKAGES=%PACKAGES%,wget,tar,gawk,bzip2,subversion
  
