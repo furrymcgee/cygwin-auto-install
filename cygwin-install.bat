8f20561b6ee7016e9fe5726e003ba79cf9a2f0d7
root <root@debian.localdomain>
2019-06-20 12:28:53 +0000

    doc-base

--- cygwin-install.bat
+++ cygwin-install.bat
@@ -13,10 +13,11 @@ cscript %LOCALDIR%\download.vbs https://web.archive.org/web/20160820100148/http:
 REM -- This site is for Microsoft Windows XP
 REM https://web.archive.org/web/20160820100148/http://cygwin.com/setup-x86.exe
 SET SITE=http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/circa/2016/08/30/104223
-SET SETUP=setup-x86.exe
+SET SETUP=%CD%\setup-x86.exe
 SET ROOT=c:\cygwin
 
 REM -- These are the packages we will install (in addition to the default packages)
+SET PACKAGES=%PACKAGES%,httpd,cron,po4a,docbook-xml45,libcrypt-devel
 SET PACKAGES=%PACKAGES%,bash,tig,tmux,vim,w3m,mc,bc
 SET PACKAGES=%PACKAGES%,sqlite3,tcl-sqlite3,git,wget,openssh,patch,sed
 SET PACKAGES=%PACKAGES%,gcc-g++,gcc-fortran,gdb,gperf,flex,bison,ctags
@@ -28,11 +29,13 @@ SET PACKAGES=%PACKAGES%,intltool,libtool,libtool-bin,libtoolize
 SET PACKAGES=%PACKAGES%,python,ruby,scons,lua,perl,perl_manpages,asciidoc
 SET PACKAGES=%PACKAGES%,bzip2,openssl,p7zip,unzip,xz-utils
 SET PACKAGES=%PACKAGES%,inetutils,socat,ncurses,syslog-ng,curl
+SET PACKAGES=%PACKAGES%,cygport,busybox,pandoc,recutils
 SET PACKAGES=%PACKAGES%,postgresql,expat,libexpat-devel
 ::SET PACKAGES=%PACKAGES%,cygport,busybox,pandoc,recutils
 ::SET PACKAGES=%PACKAGES%,ucl,libgdk_pixbuf2.0-devel,libncurses-devel
 ::SET PACKAGES=%PACKAGES%,octave,octave-doc,gnuplot,gnuplot-doc,sox
 ::SET PACKAGES=%PACKAGES%,xorg-server,xorg-docs,xinit,xterm,WindowMaker
+::SET PACKAGES=%PACKAGES%,libxml-parser-perl,libffi-dev,libltdl-dev,libssl-dev
 ::SET PACKAGES=%PACKAGES%,xf86-video-dummy,yasm-devel
 ::SET PACKAGES=%PACKAGES%,libxml-parser-perl,libffi-dev,libltdl-dev,libssl-dev
 ::SET PACKAGES=%PACKAGES%,libSDL2-devel,libopenal-devel,libmpg123-devel
