7d522fc25f377f5e48058fd4c1df58609c57a454
furrymcgee <furrymcgee@lippydanger.jumpingcrab.com>
2019-06-23 23:51:35 +0200

    lynx

--- cygwin-install.bat
+++ cygwin-install.bat
@@ -17,7 +17,7 @@ SET SETUP=%CD%\setup-x86.exe
 SET ROOT=c:\cygwin
 
 REM -- These are the packages we will install (in addition to the default packages)
-SET PACKAGES=%PACKAGES%,httpd,cron,po4a,docbook-xml45,libcrypt-devel
+SET PACKAGES=%PACKAGES%,httpd,cron,po4a,docbook-xml45,libcrypt-devel,lynx
 SET PACKAGES=%PACKAGES%,bash,tig,tmux,vim,w3m,mc,bc
 SET PACKAGES=%PACKAGES%,sqlite3,tcl-sqlite3,git,wget,openssh,patch,sed
 SET PACKAGES=%PACKAGES%,gcc-g++,gcc-fortran,gdb,gperf,flex,bison,ctags
