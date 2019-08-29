500f9990207bfb200b5710db007d81826c8310bf
furrymcgee <furrymcgee@lippydanger.jumpingcrab.com>
2019-08-29 21:11:47 +0200

    cut

--- setup/autogen.sh
+++ setup/autogen.sh
@@ -2,40 +2,118 @@
 0<<-'BASH' \
 5<<-'HINT' \
 6<<'MAKE' \
-14<<-'SOURCE' \
-15<<-'SERVICES' \
-16<<-'SETUP' \
-17<<-'EXTSOURCE' \
-LANG=C.UTF-8 bash
-	export SITE=http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/circa/2016/08/30/104223 
+7<<-'SED' \
+LANG=C bash -e
+	: sed -i /etc/xinetd.d/ftpd \
+		-e /disable/s/yes/no/ \
+		-e /user/s/cyg_server/Administrator/
+
+	while read SERVICE
+	do
+		: ${SERVICE}-config --yes
+		: cygrunsvr -S ${SERVICE}
+	done \
+	<<-'SERVICE'
+		xinetd
+		syslogd
+	SERVICE
+
+	SITE=http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/circa/2016/08/30/104223 
 
 	cd *cygwin* || exit 1
-	
-	##### DOWNLOAD #####
 
-	##### DOWNLOAD SETUP.INI #####
 	# get source packages of downloaded binaries
-	find x86/setup.ini -quit -maxdepth 0 ||
-	wget --continue --directory-prefix=x86 ${SITE}/x86/setup.ini
-	
-	##### DOWNLOAD PACKAGES #####
-	source <(
-		: cat <&15
-		cat <&16
-		: cat <&17
-	)
-	
+	: wget -P x86 ${SITE}/x86/setup.ini -c
+
+	# join setup.ini with directories
+	: join <( 
+		grep ^@\\\|^install:\\\|^source: */setup.ini |
+		sed s/^@/:@/ |
+		cut -d: -f2 | 
+		sed -z \
+			-e s/\\\n@\ /\\\x0/g \
+			-e s/\\\n/\ /g | \
+		tr \ \\\000 \\\t\\\n |
+		cut -f-3,7 | 
+		sort
+	) <(
+		find * \
+			-maxdepth 0 \
+			-mindepth 0 \
+			-type d \
+			-execdir \
+				find {}/release \
+					-mindepth 1 \
+					-type d \
+					-printf %f\\\t%H\\\n \
+				\; | 
+		sed 's/.*/echo $(basename &) $(dirname &)/e' |
+		sort
+	) | 
+	cat && exit
+	grep -o [[:graph:]]\\\+ | 
+	sed -n -f <( cat <&7 ) |
+	uniq |
+	tr \\\t \\\n |
+	sed 's%.*%echo wget -c -P *cygwin*/$(dirname &) http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/circa/2016/08/30/104223/&%' |
+	sh | sh
+
+
+	# find setup.hint and print file name, starting point and directory
+	find * \
+		-maxdepth 0 \
+		-mindepth 0 \
+		-type d \
+		-execdir \
+			find {}/release \
+				-name setup.hint \
+				-printf %p\\\t{}\\\t%h\\\n \
+			\; | 
+	sort |
+	tee >(
+		# grep external-source from setup.hint
+		cut -f3- |
+		grep emacs |
+		xargs -I@ grep --with-filename external-source @/setup.hint |
+		tr : \\\t | tr -d ' ' |
+		join -o1.1,1.3,2.1 -t$'\t' -12 - <(cat <<<external-source) 
+	) > >(
+		# tee find output
+		cat
+	) |
+	# sort by file name
+	sort |
+	{
+		# self join stdin
+		coproc { cat; }
+		exec 3<&${COPROC[0]}- 4<&${COPROC[1]}-
+		coproc { cat; }
+		exec 5<&${COPROC[0]}- 6<&${COPROC[1]}-
+		tee >(cat >&4) > >(cat >&6) &
+		exec 4>&- 6>&-
+		join -t$'\t' <(<&3 cat) <(<&5 cat) 
+	} |
+	# grep external source joined with find output
+	sort -k3 |
+	join -t$'\t' -13 - <(cat <<<external-source) |
+	cut -f2- |
+	sort -k5 |
+	join -t$'\t' -o1.3,1.2,1.1,0 -14 - <(cat <<<external-source) -v1 |
+	cat && exit
+	nl |
+	cut -d: -f1,3 |
+	uniq |
+	tr -d ' ' |
+	xargs -I@ echo wget ${SITE}/@
 	exit
 
-	find -mindepth 2 -maxdepth 2 -name setup.ini |
-	xargs --no-run-if-empty mv -bvt . &&
-	sed s/^.// <&6 |
-	: make -f - x86/release/custompackage-0.0.1-1 x86/setup.ini
+	sed s/^\\\t// <&6 |
+	make -C *cygwin* -f - x86/release/custompackage-0.0.1-1 x86/setup.ini
 BASH
-	sdesc: "My custom package"
-	ldesc: "My custom package"
+	sdesc: "My favorite packages"
+	ldesc: "My favorite packages"
 	category: Base
-	requires: bzip2 cygwin-doc file less openssh pinfo rxvt wget
+	requires: bzip2 clear cygwin-doc file less openssh pinfo rxvt wget
 HINT
 	PACKAGE=$(lastword $(subst /, ,$(firstword $(subst -, ,$@))))
 	VERSION=$(word 2,$(subst -, ,$@))
@@ -59,193 +137,15 @@ HINT
 			# --disable-check=missing-depended-package \
 			# --disable-check=missing-curr \
 
-		stat $@
-		bzip2 < $@ > $(dir $@)/setup.bz2
+		stat $@ && \
+		bzip2 < $@ > $(dir $@)/setup.bz2 && \
 		xz -6e < $@ > $(dir $@)/setup.xz
 MAKE
-	db	./x86/release
-	emacs	./x86/release
-	error/libgpg-error-devel	./x86/release/libgpg
-	ibwebpdecoder1	./x86/release/libwebp
-	libGLU1	./x86/release/glu
-	libdconf1	./x86/release/dconf
-	libe2p2	./x86/release/e2fsprogs
-	libev4	./x86/release/libev
-	libgcrypt-devel	./x86/release/libgcrypt
-	libglut3	./x86/release/freeglut
-	libgmpxx4	./x86/release/gmp
-	liblz4_1	./x86/release/lz4
-	liblzo2-doc	./x86/release/liblzo2
-	libpcre16_0	./x86/release/pcre
-	libpcre2_32_0	./x86/release/pcre2
-	libpcreposix0	./x86/release/pcre
-	libplot2	./x86/release/plotutils
-	libprocps-ng4	./x86/release/procps-ng
-	libreadline-devel	./x86/release/readline
-	libsigsegv-devel	./x86/release/libsigsegv
-	libss2	./x86/release/e2fsprogs
-	libwebpdemux1	./x86/release/libwebp
-	perl-CGI	./noarch/release
-	postgresql-client	./x86/release/postgresql
-SOURCE
-
-	##### START SERVICES #####
-
-	: sed -i /etc/xinetd.d/ftpd \
-		-e /disable/s/yes/no/ \
-		-e /user/s/cyg_server/Administrator/
-
-	while read SERVICE
-	do
-		: ${SERVICE}-config --yes
-		: cygrunsvr -S ${SERVICE}
-	done \
-	<<-'SERVICE'
-		xinetd
-		syslogd
-	SERVICE
-SERVICES
-	# downloaded setup.hint of installed packages and external sources
-	find -mindepth 2 -maxdepth 2 -name setup.ini |
-	xargs -r grep ^@\\\|^install:\\\|^source: |
-	sed s/^@/:@/ |
-	cut -d: -f2 | 
-	sed -z \
-		-e s/@\ /\\\x0/g \
-		-e s/\\\n/\ /g |
-	tr \ \\\000 \\\t\\\n |
-	cut -f1,3,5,7,9 | 
-	grep ^. |
-	sort |
-	{
-		coproc { 
-			coproc { grep setup.hint; }
-			exec 11<&${COPROC[0]}- 12<&${COPROC[1]}-
-
-			# find required setup.hint
-			join -o2.2,2.1,1.2,1.4 -t$'\t' - <(
-				cat <&14 - <(
-					find * \
-						-maxdepth 0 \
-						-mindepth 0 \
-						-type d \
-						-execdir \
-							find {}/release \
-								-mindepth 1 \
-								-type d \
-								-printf %f\\\t%h\\\t%d\\\n \
-							\;
-				) |
-				sort |
-				uniq
-			) |
-			sed \
-				-e s%\\\t%/% \
-				-e s%\\\t%/setup.hint\&% \
-				-e s%\\\t%\&./%g |
-			tr \\\t \\\n |
-			sort |
-			uniq |
-			# exclude existing setup.hint
-			join -v1 -t$'\n' - <(
-				find -mindepth 2 -type f -name setup.hint |
-				sort |
-				tee >(cat >&12)
-			)
-			cat <&11 > /dev/null &
-			exec 12>&-
-			wait
-		}
-		exec 5<&${COPROC[0]}- 6<&${COPROC[1]}-
-
-		tee >(cat >&6) | 
-		# available packages
-		cut -f2-5 |
-		tr \\\t \\\n |
-		tac |
-		paste - - |
-		sed s%\\\t%\ \ ./% |
-		sort -k2 |
-		uniq |
-		join -v1 -j2 - <(
-			find -type f | 
-			sort -k2
-			# xargs -P 0 -I@ sha512sum @
-		) |
-		sort |
-		join -v2 - <(
-			# required archives and setup.hint
-			sort <&5 &
-		) |
-		xargs -I@ printf \
-			wget\ \
-				--continue\ \
-				--directory-prefix=\$\(dirname\ %q\)\ \
-				${SITE}/%q\\\n \
-				@ @ &
-		exec 6>&- 8>&-
-
-		wait
-	} |
-	cat >&2
-	exit
-
-SETUP
-	##### EXTERNAL SOURCES #####
-
-	# use setup.hint and print file name, starting point and directory
-	find * \
-		-maxdepth 0 \
-		-mindepth 0 \
-		-type d \
-		-execdir \
-			find {}/release \
-				-name setup.hint \
-				-printf %p\\\t{}\\\t%h\\\n \
-			\; | 
-	sort |
-	# print filename/arch and grep external-source from setup.hint
-	{
-		# print filename and arch
-		coproc { cat; }
-		exec 3<&${COPROC[0]}- 4<&${COPROC[1]}-
-		# pipe filename to xargs grep
-		coproc {
-			cut -f3- |
-			xargs -I@ grep --with-filename external-source @/setup.hint |
-			tr : \\\t | tr -d ' ' |
-			join -o1.1,1.3,2.1 -t$'\t' -12 - <(cat <<<external-source) 
-		}
-		exec 5<&${COPROC[0]}- 6<&${COPROC[1]}-
-		tee >(cat >&4) > >(cat >&6) &
-		exec 4>&- 6>&-
-		cat <(<&3 cat) &
-		cat <(<&5 cat) &
-		wait
-	} |
-	sort |
-	# cross join filename/arch and external-source
-	{
-		coproc { cat; } && exec 3<&${COPROC[0]}- 4<&${COPROC[1]}-
-		coproc { cat; } && exec 5<&${COPROC[0]}- 6<&${COPROC[1]}-
-		tee >(cat >&4) > >(cat >&6) & exec 4>&- 6>&-
-		join -t$'\t' <(<&3 cat) <(<&5 cat) 
-	} |
-	sort -k3 |
-	# combine lines with external-source and starting point
-	join -t$'\t' -13 - <(cat <<<external-source) |
-	# select only lines with external-source and starting point
-	join -t$'\t' -15 -v1 -o1.4,1.3 - <(cat <<<external-source) |
-	sed s%\\\t%/release/% |
-	uniq |
-	xargs -I@ printf %q/setup.hint\\\n @ |
-	join -v1 - <(cat <&11) |
-
-	# download external-source
-	xargs -I@ \
-		printf wget\ \
-			--continue\ \
-			--directory-prefix=%q\ \
-			${SITE}/%q/setup.hint\\\n \
-			@ @ 
-EXTSOURCE
+	s%$%/setup.hint\t%
+	x; n; N
+	s%\n%\t%g
+	H; n; G
+	s%\n%/%g
+	s%\t/%\t%g
+	p
+SED
